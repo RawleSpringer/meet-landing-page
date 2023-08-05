76a0f3b69ff58fafa69a962d135ab724773ffe43
Rawle Springer <rspringerdeveloper@gmail.com>
2023-08-05 08:12:46 -0400

    html layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,43 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import '../app.css';
+</script>
+
+<div>
+  <header>e</header>
+  <main>
+    <section>
+      <h1>Group Chat For Everyone</h1>
+      <p>
+        Meet makes it easy to connect with others face-to-face virtually and
+        collaborate across any device.
+      </p>
+      <div>
+        <button>Download<span>v1.3</span></button>
+        <button>What is it?</button>
+      </div>
+    </section>
+    <section>
+      <ul>
+        <li>1</li>
+      </ul>
+
+      <div>
+        <p>Built for modern use</p>
+        <h2>Smarter meetings, all in one place</h2>
+        <p>
+          Send messages, share files, show your screen, and record your meetings
+          — all in one workspace. Control who can join with invite-only team
+          access, data encryption, and data export.
+        </p>
+      </div>
+    </section>
+  </main>
+  <footer>
+    <p>Experience more together</p>
+    <p>
+      Stay connected with reliable HD meetings and unlimited one-on-one and
+      group video sessions.
+    </p>
+    <button>Download v1.3</button>
+  </footer>
+</div>
